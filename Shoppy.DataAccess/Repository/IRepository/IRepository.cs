5570ec7669cea9dc4e4badbca63d6a6c175beae1
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2023-06-12 11:15:33 -0500

    Created an interface to stop all classes from direct DB access

---
+++ Shoppy.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppy.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        // Common Methods to Consider
+        // GET ALL, GET by ID First or Default, ADD, REMOVE, REMOVERANGE...etc,
+
+        void add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
+    }
+}
---
+++ Shoppy.DataAccess/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shoppy.DataAccess.Data;
+using Shoppy.DataAccess.Repository.IRepository;
+using Shoppy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppy.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDBContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDBContext db)
+        {
+            _db = db;
+            this.dbSet = db.Set<T>();
+        }
+
+        public void add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
