8173b272c4002ed3a1c61ef88766927987ab1eb9
Darren Soulier <56926037+Kichuwa@users.noreply.github.com>
2023-07-05 15:43:11 -0500

    Completed ShoppingCart and implemented an order summary page skeleton.

--- ShoppyApp/Pages/Customer/Cart/Index.cshtml.cs
+++ ShoppyApp/Pages/Customer/Cart/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NuGet.Versioning;
 using Shoppy.DataAccess.Repository.IRepository;
 using Shoppy.Models;
 using System.Security.Claims;
@@ -11,10 +12,12 @@ namespace ShoppyApp.Pages.Customer.Cart
     public class IndexModel : PageModel
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        public double CartTotal { get; set; }
         private readonly IUnitOfWork _unitOfWork;
         public IndexModel(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+            CartTotal = 0;
         }
 
 
@@ -26,7 +29,41 @@ namespace ShoppyApp.Pages.Customer.Cart
             {
                 ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(filter: u => u.ApplicationUserId == claim.Value,
                     includeProperties:"MenuItem,MenuItem.Food,MenuItem.Category");
+                foreach(var cart in ShoppingCartList)
+                {
+                    CartTotal += (cart.MenuItem.Price * cart.Count);
+                }
             }
+        }
+
+        public IActionResult OnPostPlus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault( u => u.Id == cartId );
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        public IActionResult OnPostMinus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault( u => u.Id == cartId );
+            if (cart.Count == 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+            }
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        public IActionResult OnPostRemove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault( u => u.Id == cartId );
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            return RedirectToPage("/Customer/Cart/Index");
         }
     }
 }
